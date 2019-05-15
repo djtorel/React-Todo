fcb41815c196d761b78f10502c687df1bf01e95d
Dominic Torruellas <dom@domtorr.com>
2019-05-14 19:29:04 -0700

    changed TodoList to render based on ternary result

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -2,14 +2,11 @@ import React from 'react';
 
 import Todo from './Todo';
 
-const renderTodos = todoList => {
-  if (todoList.length > 0) {
-    return todoList.map(todo => <Todo todo={todo} key={todo.id} />);
-  } else {
-    return <h2>There are no Todos, please enter one below!</h2>;
-  }
-};
-
-const TodoList = ({ todoList }) => renderTodos(todoList);
+const TodoList = ({ todoList }) =>
+  todoList.length > 0 ? (
+    todoList.map(todo => <Todo todo={todo} key={todo.id} />)
+  ) : (
+    <h2>There are no Todos, please enter one below!</h2>
+  );
 
 export default TodoList;
